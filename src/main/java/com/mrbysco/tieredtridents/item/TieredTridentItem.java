166c94a5143f4ca4e85e070e0d741cdd990a9df7
Mrbysco <Mrbysco@users.noreply.github.com>
2026-03-22 11:49:29 +0100

    Fix durability

--- src/main/java/com/mrbysco/tieredtridents/item/TieredTridentItem.java
+++ src/main/java/com/mrbysco/tieredtridents/item/TieredTridentItem.java
@@ -7,18 +7,14 @@ import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TridentItem;
-import net.minecraft.world.item.component.Tool;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 
-import java.util.List;
 import java.util.function.Consumer;
 
 public class TieredTridentItem extends TridentItem {
@@ -26,17 +22,11 @@ public class TieredTridentItem extends TridentItem {
 
 	public TieredTridentItem(Properties properties, Tier tier) {
 		super(properties
+				.durability(tier.getUses())
 				.component(DataComponents.TOOL, TridentItem.createToolProperties())
 				.attributes(SwordItem.createAttributes(tier, 3, -2.4F))
 		);
 		this.tier = tier;
-	}
-
-	public static Tool createToolProperties() {
-		return new Tool(List.of(
-				Tool.Rule.minesAndDrops(List.of(Blocks.COBWEB), 15.0F),
-				Tool.Rule.overrideSpeed(BlockTags.SWORD_EFFICIENT, 1.5F)
-		), 1.0F, 2);
 	}
 
 	@Override
